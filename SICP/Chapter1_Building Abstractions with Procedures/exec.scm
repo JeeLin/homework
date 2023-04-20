92fbaa13daa96cbdb660c95ae634c23328fb90e2
jjee <jeelin_zhang@163.com>
2023-04-21 00:26:27 +0800

    [SICP]2023-04-31

--- SICP/Chapter1_Building Abstractions with Procedures/exec.scm
+++ SICP/Chapter1_Building Abstractions with Procedures/exec.scm
@@ -221,3 +221,49 @@
 ; [生成函数计算斐波那契数列通项](https://blog.csdn.net/china_xyc/article/details/90115657)
 ; [斐波那契与黄金分割数](https://blog.csdn.net/china_xyc/article/details/104279724)
 
+
+;1.14
+;P29
+; (define (count-change amount)
+;   (cc amount 5))
+
+; (define (cc amount kinds-of-coins)
+;   (cond ((= amount 0) 1)
+;         ((or (< amount 0) 
+;              (= kinds-of-coins 0)) 
+;          0)
+;         (else 
+;          (+ (cc amount (- kinds-of-coins 1))
+;             (cc (- amount (first-denomination 
+;                            kinds-of-coins))
+;                 kinds-of-coins)))))
+
+; (define (first-denomination kinds-of-coins)
+;   (cond ((= kinds-of-coins 1) 1)
+;         ((= kinds-of-coins 2) 5)
+;         ((= kinds-of-coins 3) 10)
+;         ((= kinds-of-coins 4) 25)
+;         ((= kinds-of-coins 5) 50)))
+         
+;                                                     (cc 11 5)
+;                                               (cc 11 4) + (cc (- 11 50) 5)=0
+;                                         (cc 11 3) + (cc (- 11 25) 4)=0
+;                         (cc 11 2)           +                                                                                  (cc (- 11 10) 3)
+;        (cc 11 1)            +                   (cc (- 11 5) 2)                                                              (cc 1 2) + (cc (- 1 10) 3)=0
+;(cc 11 0)=0 + (cc (- 11 1) 1)                  (cc 6 1) +                              (cc (- 6 5) 2)                    (cc 1 1) + (cc (- 1 5) 2)=0 
+;         (cc 10 0)=0 + (cc (- 10 1) 1)  (cc 6 0)=0 + (cc (- 6 1) 1)                  (cc 1 1) + (cc (- 1 5) 2)=0  (cc 1 0)=0 + (cc (- 1 1) 1)=1
+;                   (cc 9 0)=0 + (cc (- 9 1) 1)   (cc 5 0)=0 + (cc (- 5 1) 1)  (cc 1 0)=0 + (cc (- 1 1) 1)=1
+;                            (cc 8 0)=0 + (cc (- 9 1) 1)   (cc 4 0)=0 + (cc (- 4 1) 1)
+;                                     (cc 7 0)=0 + (cc (- 8 1) 1)   (cc 3 0)=0 + (cc (- 3 1) 1)
+;                                              (cc 7 0)=0 + (cc (- 7 1) 1)   (cc 2 0)=0 + (cc (- 2 1) 1)
+;                                                       (cc 6 0)=0 + (cc (- 6 1) 1)    (cc 1 0)=0 + (cc (- 1 1) 1)=1
+;                                                                (cc 5 0)=0 + (cc (- 5 1) 1)
+;                                                                         (cc 4 0)=0 + (cc (- 4 1) 1)
+;                                                                                  (cc 3 0)=0 + (cc (- 3 1) 1)
+;                                                                                           (cc 2 0)=0 + (cc (- 2 1) 1)
+;                                                                                                    (cc 1 0)=0 + (cc (- 1 1) 1)=1
+
+; 存储大小就是节点总数 2^0+2^1+...+2^n(即树高11)
+; 计算大小就是叶子总数 2^n
+
+;1.15
